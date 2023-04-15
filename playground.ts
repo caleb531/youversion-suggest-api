ca6272e4435bc56f13fdc337cfb12ee97a15c074
Caleb Evans <caleb@calebevans.me>
2023-04-14 20:37:40 -0700

    Use Matthew 4:1 for example search query

--- playground.ts
+++ playground.ts
@@ -2,9 +2,8 @@ import { fetchReferenceContent } from './src/ref-content-fetcher';
 
 async function main() {
   console.log(
-    await fetchReferenceContent('psa.23.1', {
+    await fetchReferenceContent('mat 4:1', {
       language: 'eng',
-      version: 59,
     })
   );
 }
