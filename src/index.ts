7855a6292141454c790bde131411e2e5cd172c4e
Caleb Evans <caleb@calebevans.me>
2023-04-15 18:24:31 -0700

    Expose functions to get Bible data and metadata

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { getFirstReferenceMatchingName, getReferencesMatchingName } from './lookup-reference';
 export { fetchReferenceContent } from './ref-content-fetcher';
 export { getReferencesMatchingPhrase } from './search-result-fetcher';
+export { getBibleBookMetadata, getBibleData } from './utilities';
