942c43f81459d2f35803b278a8aae1521b9321cd
Caleb Evans <caleb@calebevans.me>
2023-04-22 19:16:30 -0700

    Add tests for edge cases

---
+++ tests/edge.spec.ts
@@ -0,0 +1,57 @@
+import { expect } from 'chai';
+import { getReferencesMatchingName } from '../src';
+
+describe('getReferencesMatchingName', () => {
+  it('should not match empty input', async () => {
+    const references = await getReferencesMatchingName('');
+    expect(references).to.have.length(0);
+  });
+
+  it('should not match entirely non-alphanumeric input', async () => {
+    const references = await getReferencesMatchingName('!!!');
+    expect(references).to.have.length(0);
+  });
+
+  it('should ignore excessive whitespace', async () => {
+    const references = await getReferencesMatchingName('  romans  8  28  nl  ');
+    expect(references[0].name).to.equal('Romans 8:28');
+    expect(references).to.have.length(1);
+  });
+
+  it('should ignore non-alphanumeric characters', async () => {
+    const references = await getReferencesMatchingName('!1@co#13$4^7&es*');
+    expect(references[0].name).to.equal('1 Corinthians 13:4-7');
+    expect(references).to.have.length(1);
+  });
+
+  it('should ignore trailing non-matching alphanumeric characters', async () => {
+    const references = await getReferencesMatchingName('2 co 3 x y z 1 2 3');
+    expect(references[0].name).to.equal('2 Corinthians 3');
+    expect(references).to.have.length(1);
+  });
+
+  // TODO: fix this test
+
+  // it('should recognize accented Unicode characters', async () => {
+  //   const references = await getReferencesMatchingName('é 3', {
+  //     language: 'spa',
+  //     fallbackVersion: 128
+  //   });
+  //   expect(references[0].name).to.equal('Éxodo 3');
+  //   expect(references).to.have.length(1);
+  // });
+
+  it('should normalize Unicode characters', async () => {
+    const references = await getReferencesMatchingName('e\u0301');
+    expect(references).to.have.length(0);
+  });
+
+  it('should match numbered books even if book name contains punctuation ', async () => {
+    const references = await getReferencesMatchingName('1 ch', {
+      language: 'deu',
+      fallbackVersion: 51
+    });
+    expect(references[0].name).to.equal('1. Chronik 1');
+    expect(references).to.have.length(1);
+  });
+});
