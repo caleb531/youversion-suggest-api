ff9b07dcc5e561f8325ead04051adafd401a0cee
Caleb Evans <caleb@calebevans.me>
2023-04-24 12:14:05 -0700

    Bring lookup-reference statement coverage to 100%

---
+++ tests/get-first-reference.spec.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import { getFirstReferenceMatchingName } from '../src';
+
+describe('getFirstReferenceMatchingName book logic', () => {
+  it('should match reference without options', async () => {
+    const reference = await getFirstReferenceMatchingName('mat 11.28-30');
+    expect(reference).to.have.property('name', 'Matthew 11:28-30');
+    expect(reference.version).to.have.property('name', 'NIV');
+  });
+
+  it('should match reference with options', async () => {
+    const reference = await getFirstReferenceMatchingName('éx 3.14', {
+      language: 'spa',
+      fallbackVersion: 'rvc'
+    });
+    expect(reference).to.have.property('name', 'Éxodo 3:14');
+    expect(reference.version).to.have.property('name', 'RVC');
+  });
+});
