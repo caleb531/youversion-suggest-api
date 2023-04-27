ffe83a55cbd614c1a3e72ea5794e8b0c40453f49
Caleb Evans <caleb@calebevans.me>
2023-04-26 20:39:20 -0700

    Streamline element loop in search result fetcher

--- src/search-result-fetcher.ts
+++ src/search-result-fetcher.ts
@@ -16,16 +16,17 @@ export async function parseContentFromHTML(
   const $ = cheerio.load(html);
   const $references = $("a[href*='/bible/']");
 
-  const results: BibleReference[] = [];
-  $references.each((r, referenceElem) => {
-    const $reference = $(referenceElem);
-    const reference = buildBibleReferenceFromID(getReferenceIDFromURL($reference.prop('href')), options);
-    reference.content = $reference.parent().next('p').text().trim();
-    if (reference) {
-      results.push(reference);
-    }
-  });
-  return results;
+  return Array.from($references)
+    .map((referenceElem) => {
+      const $reference = $(referenceElem);
+      const referenceId = getReferenceIDFromURL($reference.prop('href'));
+      const reference = buildBibleReferenceFromID(referenceId, options);
+      return {
+        ...reference,
+        content: $reference.parent().next('p').text().trim()
+      };
+    })
+    .filter(Boolean);
 }
 
 // Fetch the textual content of the given Bible reference; returns a promise
