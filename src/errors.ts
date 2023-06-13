c07484c0baa58cbab0cae0462bbda77d5c9b33df
Caleb Evans <caleb@calebevans.me>
2023-06-13 15:57:22 -0700

    Use correct dynamic name for BibleReferenceError class
    This is just the string name of the class, and only shows up when
    logging or debugging.

--- src/errors.ts
+++ src/errors.ts
@@ -1,5 +1,10 @@
 // The base class for errors related to Bible references
-export class BibleReferenceError extends Error {}
+export class BibleReferenceError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
 // The error thrown when a Bible reference is not found or non-existent
 export class BibleReferenceNotFoundError extends BibleReferenceError {}
 // The error thrown when the content for a Bible reference could not be parsed
