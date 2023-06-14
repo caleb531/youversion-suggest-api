19df41efd9c4ea076645395de204cea2802b470b
Caleb Evans <caleb@calebevans.me>
2023-06-14 15:16:29 -0700

    Add note about tsx loader bug on Node 20

--- ava.config.js
+++ ava.config.js
@@ -2,5 +2,11 @@ export default {
   extensions: {
     ts: 'module'
   },
-  nodeArguments: ['--loader=tsx', '--require=source-map-support/register', '--no-warnings']
+  // ESM loaders (via the --loader or --experimental-loader flag) are still
+  // experimental and may break in future versions of Node; for example, Node
+  // v20 made a breaking change that isolates each loader into its own separate
+  // processe, thereby breaking the ability to import TypeScript modules in Node
+  // (at least for the time being); see
+  // <https://github.com/TypeStrong/ts-node/issues/1997> for more details
+  nodeArguments: ['--experimental-loader=tsx', '--require=source-map-support/register', '--no-warnings']
 };
