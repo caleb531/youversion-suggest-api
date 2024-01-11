48116698f5985839ac4e95d3507b6a4fec63ab09
Caleb Evans <caleb@calebevans.me>
2024-01-10 17:25:31 -0800

    Use default Vitest reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    reporters: ['basic'],
     environment: 'node',
     coverage: {
       reporter: ['text', 'lcov', 'html', 'text-summary']
