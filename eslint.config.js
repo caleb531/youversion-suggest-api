cd0ecf5e363b3a39fdc72dd8877b7531147fd488
Caleb Evans <caleb@calebevans.me>
2024-08-17 13:54:37 -0700

    Upgrade eslint to v9 with flat config

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+import ts from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    ignores: [
+      '.DS_Store',
+      'node_modules',
+      'build',
+      'dist',
+      'coverage',
+      '.vercel',
+      'package',
+      '.env',
+      '.env.*',
+      '!.env.example',
+      'pnpm-lock.yaml',
+      'package-lock.json',
+      'yarn.lock'
+    ]
+  }
+];
