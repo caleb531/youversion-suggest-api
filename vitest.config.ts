e03a183955661af1077345a49447faa37fff5df7
Caleb Evans <caleb@calebevans.me>
2024-08-29 10:38:02 -0700

    Remove unused htmlrewriter alias in Vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,10 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
-
-// __dirname is not available in ES modules natively, so we must define it
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   test: {
     environment: 'node',
     coverage: {
       reporter: ['text', 'lcov', 'html', 'text-summary']
-    },
-    alias: {
-      // htmlrewriter: path.resolve(__dirname, 'node_modules/htmlrewriter/node.mjs')
     }
   }
 });
